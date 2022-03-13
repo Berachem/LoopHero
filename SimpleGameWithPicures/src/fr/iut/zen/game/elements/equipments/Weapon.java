8acd670d690e08fa23fabbea042f429ba67b7694
Harulla <laura.leroychhon@gmail.com>
2022-03-13 17:36:12 +0100

    oui2

--- SimpleGameWithPicures/src/fr/iut/zen/game/elements/equipments/Weapon.java
+++ SimpleGameWithPicures/src/fr/iut/zen/game/elements/equipments/Weapon.java
@@ -1,5 +1,4 @@
 package fr.iut.zen.game.elements.equipments;
-import java.util.Random;
 import java.util.*;
 
 	public class Weapon extends AbstractEquipment{
@@ -13,38 +12,13 @@ import java.util.*;
 		refreshStatsRarety();
 		allStats.remove("Damage");
 	}
-  public void refreshStatsRarety(){
-		int dmg = stats.get("Damage");
-    String bonus1 = allStats.get(new Random().nextInt(allStats.size()));
-		String bonus2 = allStats.get(new Random().nextInt(allStats.size()));
-		String bonus3 = allStats.get(new Random().nextInt(allStats.size()));
-		
-		
-		if (rarety.equals("Blue")){
-      stats.put("Damage",dmg*=0.9);
-			stats.put(allStats.get(bonus1), calcValueStat(allStats.get(bonus1))/3);
-    }
-    else if(rarety.equals("Yellow")){
-			int newDmg = dmg *(new Random().nextInt((100 - 80) + 1) + 80/100);
-			stats.put("Damage",newDmg);
 
-			stats.put(allStats.get(bonus1), calcValueStat(allStats.get(bonus1))/2);
+	@Override
+	public Dictionary stats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-			String bonus2 = allStats.get(new Random().nextInt(allStats.size()));
-			stats.put(allStats.get(bonus2), calcValueStat(allStats.get(bonus2))/2);
-			
-    }else{
-			int newDmg = dmg *(new Random().nextInt((100 - 80) + 1) + 80/100);
-			stats.put("Damage",newDmg);
-
-			stats.put(allStats.get(bonus1), calcValueStat(allStats.get(bonus1))/2);
-
-			String bonus2 = allStats.get(new Random().nextInt(allStats.size()));
-			stats.put(allStats.get(bonus2), calcValueStat(allStats.get(bonus2))/2);
-
-			stats.put(allStats.get(bonus3), calcValueStat(allStats.get(bonus3))/4);
-    }
-  }
 
  
 }
