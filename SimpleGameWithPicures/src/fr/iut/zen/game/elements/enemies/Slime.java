d39443d5a21d5672c057186936d71bd22e6591e8
Berachem Markria <berachem.markria@gmail.com>
2022-03-18 18:24:28 +0100

    creation slime

---
+++ SimpleGameWithPicures/src/fr/iut/zen/game/elements/enemies/Slime.java
@@ -0,0 +1,8 @@
+package fr.iut.zen.game.elements.enemies;
+
+public class Slime implements Mobs {
+	
+	final String slimePATH = "pictures/green-slime.png";
+	// le reste jte laisse gérer stp ^^
+	
+}
